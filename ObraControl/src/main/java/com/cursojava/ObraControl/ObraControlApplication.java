f136a31f816e5dfbd37431fb8fa4f42ae398a764
lucasdelavy23 <lucasdelavy23@gmail.com>
2026-07-05 14:10:20 -0300

    Criado projeto utilizando ferramenta spring.io

---
+++ ObraControl/src/main/java/com/cursojava/ObraControl/ObraControlApplication.java
@@ -0,0 +1,13 @@
+package com.cursojava.ObraControl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ObraControlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ObraControlApplication.class, args);
+	}
+
+}
---
+++ ObraControl/src/test/java/com/cursojava/ObraControl/ObraControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursojava.ObraControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ObraControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
