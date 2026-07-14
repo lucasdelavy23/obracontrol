2eb8b01d0d5bcbf2b1eb1e944fedfea4f014df7a
lucasdelavy23 <lucasdelavy23@gmail.com>
2026-07-14 20:28:39 -0300

    Adicionado layout base nas páginas

--- src/main/java/com/cursojava/ObraControl/controller/ObrasController.java
+++ src/main/java/com/cursojava/ObraControl/controller/ObrasController.java
@@ -1,6 +1,7 @@
 package com.cursojava.ObraControl.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -11,4 +12,45 @@ public class ObrasController {
     public String obras() {
         return "obras";
     }
+
+    @GetMapping("/apartamento")
+    public String apartamento() {
+        return "apartamento";
+    }
+
+    @GetMapping("/cadastrar-instalador")
+    public String cadastrar_instalador() {
+        return "cadastrar-instalador";
+    }
+
+    @GetMapping("/cadastro-obra")
+    public String cadastro_obra() {
+        return "cadastro-obra";
+    }
+
+    @GetMapping("/checklist-apartamento")
+    public String checklist_apartamento() {
+        return "checklist-apartamento";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/lista-obra")
+    public String lita_obra() {
+        return "lista-obra";
+    }
+
+    @GetMapping("/lista-prestador")
+    public String lita_prestador() {
+        return "lista-prestador";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
 }
