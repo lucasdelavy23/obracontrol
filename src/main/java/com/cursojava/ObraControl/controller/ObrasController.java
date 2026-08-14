e012f961bd01222bbe6888a013a2feb045399f12
thiagorc11 <thiagorc11rc@gmail.com>
2026-08-13 21:46:55 -0300

    ajuste 1308

--- src/main/java/com/cursojava/ObraControl/controller/ObrasController.java
+++ src/main/java/com/cursojava/ObraControl/controller/ObrasController.java
@@ -7,8 +7,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class ObrasController {
 
-    @PostMapping("/obras")
-
+    @GetMapping("/obras")
     public String obras() {
         return "obras";
     }
